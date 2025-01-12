1aa67682e24e642350b16ddc0c3f4d6b8d5ade20
aminsheikh72 <aminsheikh9065@gmail.com>
2025-01-12 09:45:54 +0000

    final result

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let searchBtn = document.getElementById("searchBtn");
 
 const getData = async (movie) =>{
    try{
-       let fetchData = await fetch(`http://www.omdbapi.com/?apikey=${APIKey}&t=${movie}`);
+       let fetchData = await fetch(`https://www.omdbapi.com/?apikey=${APIKey}&t=${movie}`);
    let jsonData = await fetchData.json();
 
    console.log(jsonData);
